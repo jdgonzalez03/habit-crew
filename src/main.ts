96164bc37381959ae766d9e0cd44e2c7e7166b04
jdgonzalez02 <juandavidgonzalezurrego@gmail.com>
2025-10-14 10:11:42 -0500

    Refactor: Organize imports and add focustrap directive

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import '@/assets/global.css'
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
+
+import App from '@/App.vue'
+import router from '@/router'
 
 import { createApp } from 'vue'
-import App from './App.vue'
 
-import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import FocusTrap from 'primevue/focustrap';
 
 const app = createApp(App)
 app.use(router)
@@ -14,5 +16,8 @@ app.use(PrimeVue, {
     preset: Aura,
   },
 })
+
+app.directive('focustrap', FocusTrap);
+
 
 app.mount('#app')
