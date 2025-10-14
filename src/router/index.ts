9ad7e110936a74f679136345aa170057df72dac8
jdgonzalez02 <juandavidgonzalezurrego@gmail.com>
2025-10-14 10:12:14 -0500

    Feature: Add register and login pages with simple navigations guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HelloView from '@/pages/HelloView.vue'
+import useAuth from '@/composables/useAuth'
+
+
+const registerView = () => import('@/pages/RegisterView.vue')
+const loginView = () => import('@/pages/LoginView.vue')
+
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HelloView,
-    default: true,
+    meta: {
+      requiresAuth: true,
+    }
   },
+  {
+    path: '/register',
+    name: 'register',
+    component: registerView,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: loginView,
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
+
+router.beforeEach((to) => {
+  const auth = useAuth()
+
+  const isLoggedIn = auth.authIsLogin.value
+  console.log(isLoggedIn)
+
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    return { path: '/login' }
+  }
+
+  if ((to.path === '/login' || to.name === 'login' || to.path === '/register' || to.name === 'register') && isLoggedIn) {
+    return { path: '/' }
+  }
+
+  return true
+})
+
 
 export default router
